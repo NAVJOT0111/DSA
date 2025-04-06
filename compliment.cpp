e767bc61a0ef0c92f4950e0baf09307d9c775154
Navjot Kaur <navjotkaur1440@gmail.com>
2025-04-06 15:49:43 +0530

    Practice questions

---
+++ compliment.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int m,n;
+    cout << "Enter the value of n" << endl;
+    cin >> n;
+    m = n;
+    int mask = 0;
+
+    while(m){
+        mask = (mask << 1) | 1;
+        m = m >> 1;
+    }
+    int ans = (~n) & mask;
+    cout << ans << endl;
+}
